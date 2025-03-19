16651c8a05bc2fa74a2c847c6c452473aa6f95ef
youssef_74 <youssefessam1293@gmail.com>
2025-03-19 03:31:51 +0200

    Model Binding

--- WebApplication2/Controllers/MoviesController.cs
+++ WebApplication2/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
 
 namespace WebApplication2.Controllers
 {
@@ -36,11 +37,38 @@ namespace WebApplication2.Controllers
                 return Content(content: $"Movie With Name = {name} And Id = {id}", "text/html");
         }
         public IActionResult TestRedirectAction()
-            {
+        {
             return RedirectToRoute("default", new { Controller = "Movies", Action = "GetMovie", Id = 10 });
             //  return RedirectToAction(nameof(GetMovie), "movies"// lazem tktbha anta n type of bayze btkon kda moviescontroller
             //                                                   , new { id = 10, name = "batoomasr" });
             //  return Redirect("https://www.instagram.com/youssefessam_74/");
+        }
+        // baseUrl/Movies/TestModelBinding
+        [HttpPost]
+        public IActionResult TestModelBinding([FromQuery] int id, string name)
+        {
+            return Content($"hello {name} your id is {id}");
+
+        }
+        [HttpPost]
+        public IActionResult AddMovie(/*[FromBody][FromHeader] Movie movie */ [FromHeader] int Id, [FromHeader] string Title) //fromheader is not working for comples data
+        {
+            /*  if(movie is null)
+                  return BadRequest();
+              else
+            */
+            return Content($"movie with {Id} with id {Title}");
+
+        }
+        [HttpGet]
+
+        public IActionResult AddMovie(Movie movie, int Id, string Title, int[] arr) // 7dd anta b2a mn el url
+        {
+            /*  if(movie is null)
+                  return BadRequest();
+              else
+            */
+            return Content($"");
         }
     }
 }
